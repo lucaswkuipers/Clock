1b0f332b5df32db3ac073ad24997cef9942cb2f9
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2024-10-13 23:55:24 +0200

    Two lines

--- Clock/AppDelegate.swift
+++ Clock/AppDelegate.swift
@@ -4,13 +4,30 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem!
     private var timer: Timer!
     private var dateFormatter: DateFormatter!
+    private var timeFormatter: DateFormatter!
+    private var statusItemView: NSTextField!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         dateFormatter = DateFormatter()
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        statusItem.button?.font = .monospacedSystemFont(ofSize: NSFont.systemFontSize, weight: .regular)
-        dateFormatter.dateFormat = "dd/MM/yy HH:mm:ss"
+        timeFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yy"
+        timeFormatter.dateFormat = "HH:mm:ss"
 
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        statusItemView = NSTextField(labelWithString: "")
+        statusItemView.translatesAutoresizingMaskIntoConstraints = false
+        statusItemView.font = .monospacedSystemFont(ofSize: NSFont.smallSystemFontSize, weight: .regular)
+        statusItemView.alignment = .center
+        statusItemView.maximumNumberOfLines = 2
+        statusItemView.lineBreakMode = .byWordWrapping
+        statusItem.button?.addSubview(statusItemView)
+        NSLayoutConstraint.activate([
+            statusItemView.leadingAnchor.constraint(equalTo: (statusItem.button?.leadingAnchor)!),
+            statusItemView.trailingAnchor.constraint(equalTo: (statusItem.button?.trailingAnchor)!),
+            statusItemView.topAnchor.constraint(equalTo: (statusItem.button?.topAnchor)!),
+            statusItemView.bottomAnchor.constraint(equalTo: (statusItem.button?.bottomAnchor)!)
+        ])
         updateStatusItemTitle()
 
         timer = Timer.scheduledTimer(
@@ -20,13 +37,13 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             userInfo: nil,
             repeats: true
         )
-
         RunLoop.current.add(timer, forMode: .common)
     }
 
     @objc private func updateStatusItemTitle() {
-        let dateAndTimeString = dateFormatter.string(from: .now)
-        statusItem.button?.title = dateAndTimeString
+        let dateString = dateFormatter.string(from: Date())
+        let timeString = timeFormatter.string(from: Date())
+        statusItemView.stringValue = "\(dateString)\n\(timeString)"
     }
 
     func applicationWillTerminate(_ notification: Notification) {
